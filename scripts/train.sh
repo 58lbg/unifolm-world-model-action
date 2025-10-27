00ef3c82c4152f5432af451637f30b26c77eaba0
zhangke05 <zhangke05@58.com>
2025-10-27 19:30:13 +0800

    修改训练配置文件

--- scripts/train.sh
+++ scripts/train.sh
@@ -20,13 +20,13 @@ save_root="outputs/train/unifolm"
 mkdir -p $save_root/$name
 
 ## run
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch \
+CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch \
 --nproc_per_node=8 --nnodes=1 --master_addr=127.0.0.1 --master_port=12366 --node_rank=0 \
 ./scripts/trainer.py \
 --base $config_file \
 --train \
 --name $name \
 --logdir $save_root \
---devices 8 \
---total_gpus=8 \
+--devices 1 \
+--total_gpus=1 \
 lightning.trainer.num_nodes=1
